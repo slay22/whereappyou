a646cee28e12dbbce5ed1c4398bf78c05b4c943b
alexlujamail@gmail.com <alexlujamail@gmail.com@d81d8e8c-f7ea-9733-8b43-d44dabaedced>
2013-03-11 16:28:40 +0000

    Added GUI

---
+++  whereappyou/src/com/freshtechnology/whereappyou/LayoutKontakteActivity.java
@@ -0,0 +1,21 @@
+package com.freshtechnology.whereappyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutKontakteActivity extends Activity
+{
+	// Ausführen beim start
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		
+        super.onCreate(savedInstanceState);
+        
+        // Ruft das Layout auf
+        setContentView(R.layout.contacts_layout);
+        
+    }
+	
+	
+
+}
---
+++  whereappyou/src/com/freshtechnology/whereappyou/LayoutReceivedActivity.java
@@ -0,0 +1,19 @@
+package com.freshtechnology.whereappyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutReceivedActivity extends Activity
+{
+	
+	public void onCreate(Bundle savedInstanceState) 
+	{
+        super.onCreate(savedInstanceState);
+        
+        
+        setContentView(R.layout.received_layout);
+    }
+	
+	
+
+}
